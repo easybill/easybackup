33827e696fdec487430ddeaf11aa221b89175677
Patrick Romowicz <patrick@romowicz.de>
2023-05-10 09:49:43 +0200

    fix wrong argument

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 )
 
 func xtrabackup(targetDir string, incrementalBasedir string) {
-	arg := []string{"--back1up", "--target-dir=" + targetDir, "--parallel=4"}
+	arg := []string{"--backup", "--target-dir=" + targetDir, "--parallel=4"}
 	if incrementalBasedir != "" {
 		arg = append(arg, "--incremental-basedir="+incrementalBasedir)
 	}
